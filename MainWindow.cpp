c3fbd0a7e6e82032e5c7a002f41c2f76d2c1c668
Alcander-Liu <chiawei.liu.1201@gmail.com>
2017-09-08 13:48:14 +0800

    no message

---
+++ MainWindow.cpp
@@ -0,0 +1,15 @@
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    int a;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
